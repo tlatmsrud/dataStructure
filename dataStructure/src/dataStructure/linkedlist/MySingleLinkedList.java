7e96f0e94aaf6d86672bf505a670662a3e5bd6c5
tlatmsrud <tlatmsrud@naver.com>
2021-07-20 00:27:36 +0900

    20210720 - Custom LinkedList

--- dataStructure/src/dataStructure/linkedlist/MySingleLinkedList.java
+++ dataStructure/src/dataStructure/linkedlist/MySingleLinkedList.java
@@ -19,13 +19,13 @@ public class MySingleLinkedList<T> {
 	}
 	
 	public void addAfter(Node<T> before, T item) {
-		Node<T> temp = new Node<T>( item );
-		temp.next = before.next;
+		Node<T> temp = new Node<T>( item ); 
+		temp.next = before.next; 
 		before.next = temp;
 		size++;
 	}
 	
-	public T removeFirst() throws Exception {
+	public T removeFirst() {
 		if(head == null)
 			return null;
 		
@@ -42,21 +42,87 @@ public class MySingleLinkedList<T> {
 		before.next = before.next.next;
 		return temp;
 	}
+	
+	/**
+	 * index - 1번째 노드에 새로운 노드를 추가
+	 * @param index
+	 * @param item
+	 */
 	public void add(int index, T item) {
-		
+		// index > size인 이유는 마지막 인덱스 + 1번째에 추가할 수있기 때문.
+		if(index < 0 || index > size) {
+			return;
+		}
+		if(index == 0){
+			addFirst(item);
+		}else {
+			Node<T> q = getNode(index - 1);
+			addAfter(q, item);
+		}
 	}
 	
-	public void remove( int index ) {
+	public T remove( int index ) {
+		if(index < 0 || index >= size) {
+			return null;
+		}
+		if(index == 0) {
+			removeFirst();
+		}
 		
+		Node<T> prev = getNode(index -1);
+		return removeAfter(prev);
+	}
+	
+	public T remove(T item) {
+		
+		Node<T> p = head;
+		Node<T> q = null;
+		
+		while(p != null && !item.equals(p.data)) {
+			q = p;
+			p = p.next;
+		}
+		
+		if(p == null) {
+			return null;
+		}
+		if(q == null) {
+			return removeFirst();
+		}
+		return removeAfter(q);
 	}
 	
 	public int indexOf( T item ) {
+		Node<T> p = head;
+		int index = 0;
 		
+		while(p != null) {
+			if(p.data.equals(item)) {
+				return index;
+			}
+			p = p.next;
+			index ++;
+		}
 		return -1;
 	}
 	
+	public Node<T> getNode(int index) {
+		if(index < 0 || index >= size) {
+			return null;
+		}
+		Node<T> p = head;
+		for(int i =0; i<index;i++) {
+			p = p.next;
+		}
+		
+		return p;
+	}
+	
 	public T get(int index) {
-		return null;
+		if(index < 0 || index >= size) {
+			return null;
+		}
+		return getNode(index).data;
 	}
 	
 	public static void main(String[] args) {
